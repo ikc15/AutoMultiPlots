bfada69237e6f1b503b59f7a1696562fbf0d41ca
Matthew Cheng <36248844+ikc15@users.noreply.github.com>
2018-07-04 11:32:18 +0900

    Update plot_w_glob.py

--- plot_w_glob.py
+++ plot_w_glob.py
@@ -4,13 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 
-filenames = sorted(glob.glob('C:/Users/Owner/heliopy/data/helios/E1_experiment/New_proton_corefit_data_2017/ascii/helios2/1976/*.csv', recursive=True))
+filenames = sorted(glob.glob('C:/directory/*.csv', recursive=True)) # * means any file inside that directory
 filenames = filenames[0:1] # choose number of filenames you want
 for f in filenames:
     print(f)
     data = pd.read_csv(f)
     print(data.keys())
-
+    
+    # create plots of your data
     fig, axs = plt.subplots(3, 1, sharex=True)
     axs[0].plot(data['n_p'])
     axs[1].plot(data['vp_x'])
